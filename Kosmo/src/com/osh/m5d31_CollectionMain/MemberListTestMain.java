10f3702c33891d8e299cc62352a750c8adc3e388
spada9403 <62082361+spada9403@users.noreply.github.com>
2021-05-31 15:52:39 +0900

    LinkedList,Stack

--- Kosmo/src/com/osh/m5d31_CollectionMain/MemberListTestMain.java
+++ Kosmo/src/com/osh/m5d31_CollectionMain/MemberListTestMain.java
@@ -3,25 +3,25 @@ package com.osh.m5d31_CollectionMain;
 import com.osh.m5d31_Collection.Member;
 
 public class MemberListTestMain {
-	public static void main(String[] args){
+	public static void main(String[] args) {
 		MemberArrayList memberArrayList = new MemberArrayList();
-		
+
 		Member memberLee = new Member(1001, "이지원");
 		Member memberSon = new Member(1002, "손민국");
 		Member memberPark = new Member(1003, "박서훤");
 		Member memberHong = new Member(1004, "홍길동");
-		
+
 		memberArrayList.addMember(memberLee);
 		memberArrayList.addMember(memberSon);
 		memberArrayList.addMember(memberPark);
 		memberArrayList.addMember(memberHong);
-		
+
 		memberArrayList.showAllMembers();
-		
+
 		memberArrayList.removeMember(memberHong.getMamberId());
 		memberArrayList.showAllMembers();
-		
-		Member member = new Member(1004,"동동동");
+
+		Member member = new Member(1004, "동동동");
 		memberArrayList.insertMember(member, 2);
 		memberArrayList.showAllMembers();
 	}
