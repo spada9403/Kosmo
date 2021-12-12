8d192b890ca3e85156838f5481a3e7c1622d6828
spada9403 <62082361+spada9403@users.noreply.github.com>
2021-12-12 22:30:08 +0900

    Update BoardList.js

--- react/prj-react-01/src/board/BoardList.js
+++ react/prj-react-01/src/board/BoardList.js
@@ -1,7 +1,9 @@
-import React from "react";
 import React, { useState, useRef, useEffect } from "react";
 import axios from "axios";
+
 function BoardList(props) {
+  const [searchCdt, setSearchCdt] = useState({});
+  const [searchResult, setSearchResult] = useState({});
 
   return <div></div>;
 }
